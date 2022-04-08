74a625a6c5905e2524a6b453d1d8d35c9c56822d
20127037 <20127037@student.hcmus.edu.vn>
2022-04-08 22:03:43 +0700

    Test

--- Server.js
+++ Server.js
@@ -9,25 +9,25 @@ SERVER.use(express.json());
 
 const mssql = require('mssql');
 
-// const dbConfig = {
-// 	server: 'lnthuy.database.windows.net',
-// 	user: 'lnthuy29012002',
-// 	password: 'Lnthuy@29012002',
-// 	database: 'Image_Collection',
-// 	pool: {
-// 		min: 0,
-// 		max: 10,
-// 		idleTimeoutMillis: 30000
-// 	},
-// 	options: {
-// 		encrypt: true,
-// 		trustServerCertificate: false
-// 	}
-// }
+const dbConfig = {
+	server: 'lnthuy.database.windows.net',
+	user: 'lnthuy29012002',
+	password: 'Lnthuy@29012002',
+	database: 'Image_Collection',
+	pool: {
+		min: 0,
+		max: 10,
+		idleTimeoutMillis: 30000
+	},
+	options: {
+		encrypt: true,
+		trustServerCertificate: false
+	}
+}
 
 async function DB(command) {
 	try {
-		await mssql.connect('Server=tcp:lnthuy.database.windows.net,1433;Initial Catalog=Image_Collection;Persist Security Info=False;User ID=lnthuy29012002;Password=Lnthuy@29012002;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;')
+		await mssql.connect(dbConfig)
 		const result = await mssql.query(command)
 		return result
 	}
@@ -46,16 +46,15 @@ async function DB(command) {
 // 	else res.send(JSON.stringify({answer: 'false'}))
 // })
 SERVER.get('/', (req, res) => {
-	res.send("I hear you!")
-	// try {
-	// 	const stt = Math.floor(Math.random(0, 1) * 10 + 1)
-	// 	DB(`select * from Images where stt=${stt}`)
-	// 	.then(obj => res.send(JSON.stringify(obj.recordset[0])))
-	// }
-	// catch (error) {
-	// 	console.log('Cannot fetch data from Azure Database!')
-	// 	throw error
-	// }
+	try {
+		const stt = Math.floor(Math.random(0, 1) * 10 + 1)
+		DB(`select * from Images where stt=${stt}`)
+		.then(obj => res.send(JSON.stringify(obj.recordset[0])))
+	}
+	catch (error) {
+		console.log('Cannot fetch data from Azure Database!')
+		throw error
+	}
 })
 SERVER.put('/', (req, res) => {
 	DB(`update Images set numOfClick=${req.body.numOfClick} where imageURL='${req.body.imageURL}'`)
