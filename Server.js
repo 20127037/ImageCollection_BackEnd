4e7f92db090eb511d9ae3f7cd8f93088c8564004
20127037 <20127037@student.hcmus.edu.vn>
2022-04-08 09:16:13 +0700

    Change dbconfig

--- Server.js
+++ Server.js
@@ -8,24 +8,26 @@ SERVER.use(cors());
 SERVER.use(express.json());
 
 const mssql = require('mssql');
-const dbConfig = {
-	server: 'lnthuy.database.windows.net',
-	user: 'lnthuy29012002',
-	password: 'Lnthuy@29012002',
-	database: 'Image_Collection',
-	pool: {
-		min: 0,
-		max: 10,
-		idleTimeoutMillis: 30000
-	},
-	options: {
-		encrypt: true,
-		trustServerCertificate: false
-	}
-}
+
+// const dbConfig = {
+// 	server: 'lnthuy.database.windows.net',
+// 	user: 'lnthuy29012002',
+// 	password: 'Lnthuy@29012002',
+// 	database: 'Image_Collection',
+// 	pool: {
+// 		min: 0,
+// 		max: 10,
+// 		idleTimeoutMillis: 30000
+// 	},
+// 	options: {
+// 		encrypt: true,
+// 		trustServerCertificate: false
+// 	}
+// }
+
 async function DB(command) {
 	try {
-		await mssql.connect(dbConfig)
+		await mssql.connect('Server=tcp:lnthuy.database.windows.net,1433;Initial Catalog=Image_Collection;Persist Security Info=False;User ID=lnthuy29012002;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;')
 		const result = await mssql.query(command)
 		return result
 	}
